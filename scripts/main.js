5ffb0f45a73554e07ec4c732024115bdd6f064ab
kohlrabi <kohlrabi@aragonit>
2015-04-15 21:44:11 +0200

    acceleration

--- scripts/main.js
+++ scripts/main.js
@@ -18,17 +18,23 @@ NEIN.Title = {
 }
 
 NEIN.Main = {
-	create: function() {
-	},
 	enter: function() {
+		this.x = 298;
+		this.y = -64;
+		this.v = 0;
 	},
 	leave: function() {
 	},
 	step: function(dt) {
+		this.y += dt*this.v;
+		this.v += dt*40;
 	},
 	render: function(dt) {
+		var current = (this.app.lifetime % 2 / 2) * this.app.atlases.guy.frames.length | 0;
+
 		this.app.layer
-			.clear("#fff");
+			.clear("#fff")
+			.drawAtlasFrame(this.app.atlases.guy, current, this.x, this.y);
 	}
 };
 
@@ -44,6 +50,7 @@ NEIN.Score = {
 playground({
 	create: function() {
 		this.loadImage("nein");
+		this.loadAtlas("guy");	
 	},
 	ready: function() {
 		this.setState(NEIN.Title);
